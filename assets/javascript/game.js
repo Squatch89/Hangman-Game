9637fa38a68eb11339d3a449a1c443960bb9a793
Gene Williams <GeneWillb@Gmail.com>
2017-08-24 10:26:18 -0400

    modified hangman, new starting point, working on string matching

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,84 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var lettersGuessed = [];
+var allowedChoices =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var words = ["feta", "gouda", "cheddar", "swiss", "gorgonzola", "mozzarella"];
+
+//chooses a letter to be guessed
+var selectedWord = words[Math.floor((Math.random() * (words.length - 1)))];
+console.log(selectedWord + " this is the selected word");
+
+//displays _ _ _ for the length of the selected word
+function wordDisplay () {
+    var wordNode = document.createTextNode(selectedWord);
+    document.getElementById("word").appendChild(wordNode);
+    //display _ _ _ _ for each letter iof the selectedWord
+}
+
+//displays wins losses and guesses left
+var guessesLeftNode = document.createTextNode(guessesLeft);
+document.getElementById("guessesLeft").appendChild(guessesLeftNode);
+
+var winsNode = document.createTextNode(wins);
+document.getElementById("wins").appendChild(winsNode);
+
+// var lossesNode = document.createTextNode(losses);
+// document.getElementById("losses").appendChild(lossesNode);
 
 
-//press any key to begin
-//     window.onkeypress = function () {
+
+
+function resetGame () {
+    guessesLeft = 10;
+    lettersGuessed = [];
+    document.getElementById("letters").textContent = lettersGuessed;
+    document.getElementById("guessesLeft").textContent = guessesLeft;
+    selectedWord = words[Math.floor((Math.random() * (words.length - 1)))];
+    console.log(selectedWord + " this is the selected word");
+}
+
+//displays the letters guessed
+document.onkeyup = function (event) {
+    var userInput = event.key;
     
-        var wins = 0;
-        var numGuesses = 10;
-        var lettersGuessed = [];
-        
-        
-//list of words to guess
-        var words = [
-            ["e", "c", "l", "i", "p", "s", "e"],
-            ["m", "o", "o", "n"],
-            ["s", "o", "l", "a", "r"],
-            ["s", "u", "n"]
-        ];
-
-//chose word to be guessed
-        var selectedWord = words[Math.floor((Math.random() * (words.length - 1)))];
-       console.log(selectedWord);
-
-// symbolizes and displays every letter in selectedWord as _ _ _ _ _
     
-        // for (i = 0; i < selectedWord.length; i++) {
-        //     selectedWord[i] = "_ ";
-        //     console.log(selectedWord);
-        //     var displayedWord = document.getElementById("word");
-        //     var wordNode = document.createTextNode(selectedWord[i]);
-        //     displayedWord.appendChild(wordNode);
-        // }
-
-// make an object equating key codes to letters accepted
-
-        //         var alpha = {
-//             65: "a",
-//             66: "b",
-//             67: "c",
-//             68: "d",
-//             69: "e",
-//             70: "f",
-//             71: "g",
-//             72: "h",
-//             73: "i",
-//             74: "j",
-//             75: "k",
-//             76: "l",
-//             77: "m",
-//             78: "n",
-//             79: "o",
-//             80: "p",
-//             81: "q",
-//             82: "r",
-//             83: "s",
-//             84: "t",
-//             85: "u",
-//             86: "v",
-//             87: "w",
-//             88: "x",
-//             89: "y",
-//             90: "z",
-//         };
-
-
-//takes an input from the user and stores it in an array, will not repeat the same choice more than once
-        document.onkeyup = function (event) {
-            var userInput = event.key;
-            
-            if ((lettersGuessed.indexOf(userInput) < 0)) {
-            
-            
-                // console.log(typeof userInput);
-            
-                lettersGuessed.push(userInput);
-            
-                var lettersChosenNode = document.createTextNode(" " + lettersGuessed[lettersGuessed.length - 1]);
-                document.getElementById("letters").appendChild(lettersChosenNode);
-            
-            }
+    if ( (lettersGuessed.indexOf(userInput) < 0) && (allowedChoices.indexOf(userInput) >= 0) ) {
         
-            console.log(lettersGuessed + " outside of if still in keyup function");
-            
-            if (lettersGuessed[lettersGuessed.length -1] in selectedWord){
-                console.log("yay you're making progress");
-            }
-        };
+        
+        // console.log(typeof userInput + " this was the user input");
+        
+        lettersGuessed.push(userInput);
+        
+        var lettersChosenNode = document.createTextNode(" " + lettersGuessed[lettersGuessed.length - 1]);
+        document.getElementById("letters").appendChild(lettersChosenNode);
+        
+        //checks for win and loss conditions and updates the values displayed
+        if (String(lettersGuessed[lettersGuessed.length - 1]) === String(selectedWord)) {
+            wins++;
+            console.log(wins + " this was a win");
+            document.getElementById("wins").textContent = wins;
+            resetGame();
+        }
+        else
+        {
+            guessesLeft--;
+            console.log(guessesLeft + " this was a guess");
+            document.getElementById("guessesLeft").textContent = guessesLeft;
+        }
+        
+        console.log(lettersGuessed);
+        //determines if you have lost and updates score if so
+        if (guessesLeft === 0)
+        {
+            losses++;
+            console.log(losses + " this was a loss");
+            resetGame();
+        }
+    }
+    
+  
+    
+};
+
  
-//check to see if the letter guessed is in the selected word and display the letter if it is, remove a guess if it is not
-
-       
-       
-      
-
-
-
-
-//
-    // };
